72aabf47a5b7748060ef80e951ac30605c6883bb
rogebautista <rogeliobautistasanchez@outlook.com>
2023-07-19 22:16:40 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var memberBtn = document.getElementById("memberBtn");
+    var skillsBtn = document.getElementById("skillsBtn");
+    member.style.display = "block";
+    skills.style.display = "none";
+    memberBtn.style.backgroundColor = "#2c3e50";
+    skillsBtn.style.backgroundColor = "#34495e";
+}
